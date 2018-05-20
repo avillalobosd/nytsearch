c3f4c4f860aa269beffb9512f2032c1046ff4ae5
Lab Man <gustavo.canales@me.com>
2018-05-20 11:29:14 -0500

    Added the author

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,80 +1,94 @@
-$(document).ready(function () {
-    var startYear = "";
-    var endYear = "";
-    console.log(startYear);
 
-    var APIkey = "8066e48ee7f14bd4a877966c12f2b41d";
+var startYear = "";
+var endYear = "";
+console.log(startYear);
 
-
-    $(".search").click(function () {
+var APIkey = "8066e48ee7f14bd4a877966c12f2b41d";
 
 
-        var searchTerm = $("#term").val();
-        console.log(searchTerm);
-
-        var numberRecords = $("#numRec").val();
-        console.log(numberRecords);
-
-        startYear = $("#startY").val();
-        endYear = $("#endY").val();
+$(".search").click(function () {
 
 
-        var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
-        url += '?' + $.param({
-            'api-key': APIkey,
-            'q': searchTerm,
-        });
+    var searchTerm = $("#term").val();
+    console.log(searchTerm);
 
-        if (startYear != "") {
-            startYear = $("#startY").val() + "0101";
-            url += "?" + $.param({
-                "begin_date": startYear
-            });
+    var numberRecords = $("#numRec").val();
+    console.log(numberRecords);
 
-        }
-        if (endYear != "") {
-            endYear = $("#endY").val() + "1231";
-            url += "?" + $.param({
-                "end_date": endYear
-            })
+    startYear = $("#startY").val();
+    endYear = $("#endY").val();
 
-        }
 
-        $.ajax({
-            url: url,
-            method: "GET"
-        }).then(function (response) {
-            var results = response.response.docs;
-            console.log(results);
-            for (var i = 0; i < numberRecords; i++) {
-                newsIndex=i+1;
-                var newsDiv=$("<div>");
-                newsDiv.addClass("newsSection");
-                var headline=$("<p>");
-                headline.text(newsIndex + " " + results[i].headline.main);
-                newsDiv.append(headline);
-                var link=$("<a>")
-                link.attr("href",results[i].web_url);
-                link.text(results[i].web_url);
-                newsDiv.append(link);
-                $(".noticiasAqui").append(newsDiv);
-                console.log(results[i].headline.main);
-                //console.log(results[i].byline.original);
-                console.log(results[i].pub_date);
-                console.log(results[i].web_url);
-
-            }
-        });
-
+    var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+    url += '?' + $.param({
+        'api-key': APIkey,
+        'q': searchTerm,
     });
 
+    if (startYear != "") {
+        startYear = $("#startY").val() + "0101";
+        url += "?" + $.param({
+            "begin_date": startYear
+        });
 
-    $(".clear").on("click",function () {
-        $("#term").val("");
-        $("#numRec").val("");
-        $("#startY").val("");
-        $("#endY").val("");
-        $(".noticiasAqui").empty();
-    })
+    }
+    if (endYear != "") {
+        endYear = $("#endY").val() + "1231";
+        url += "?" + $.param({
+            "end_date": endYear
+        })
+
+    }
+
+    $.ajax({
+        url: url,
+        method: "GET"
+    }).then(function (response) {
+        var results = response.response.docs;
+        console.log(results);
+        console.log(results[0].hasOwnProperty("byline"));
+        for (var i = 0; i < numberRecords; i++) {
+            newsIndex = i + 1;
+            var newsDiv = $("<div>");
+            newsDiv.addClass("newsSection");
+            var headline = $("<h3>");
+            headline.text(newsIndex + ". " + results[i].headline.main);
+            newsDiv.append(headline);
+            var datePublish = $("<p>");
+            datePublish.text("Date Publish: " + results[i].pub_date);
+            newsDiv.append(datePublish);
+
+            if (results[i].hasOwnProperty("byline") == true) {
+                var author = $("<p>");
+                author.text("Author: " + results[i].byline.original);
+                newsDiv.append(author);
+
+            }
+
+            var link = $("<a>")
+            link.attr("href", results[i].web_url);
+            link.text(results[i].web_url);
+            newsDiv.append(link);
+
+            $(".noticiasAqui").append(newsDiv);
+
+            console.log(results[i].headline.main);
+            //console.log(results[i].byline.original);
+            console.log(results[i].pub_date);
+            console.log(results[i].web_url);
+
+        }
+    });
+
 });
+
+
+$(".clear").on("click", function () {
+    $("#term").val("");
+    $("#numRec").val("");
+    $("#startY").val("");
+    $("#endY").val("");
+    $(".noticiasAqui").empty();
+})
+
 
